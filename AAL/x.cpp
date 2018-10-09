3e3fa50443927e8f5a358b1aa9213b47e69b49c4
Ayushjain1722 <ayushjain1722@gmail.com>
2018-10-09 11:51:14 +0530

    Git Rocks

---
+++ AAL/x.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+
+{
+    
+int N,i=0;
+    
+cin>>N;
+    
+while(i<N)
+    
+	{	
+        
+	  cout<<"\n*   *";
+        
+	  cout<<"\n*   *";
+        
+	  cout<<"\n*****";
+        
+	  i++;
+    
+	}
+    
+	cout<<"\n*   *";
+    
+	cout<<"\n*   *";
+
+}
