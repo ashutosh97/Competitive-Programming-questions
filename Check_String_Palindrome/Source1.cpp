cefd1a2eeb88bcf364309866b43ebcb77f1d4719
Haozhe Zhang <zhz19980514@ufl.edu>
2018-10-20 12:47:31 -0400

    Update Check_String_Palindrome/Source1.cpp

--- Check_String_Palindrome/Source1.cpp
+++ Check_String_Palindrome/Source1.cpp
@@ -1,26 +1,28 @@
 #include<iostream>
 #include<string>
 
+using namespace std;
+
 bool checkStringPallindrome(string inputString);
 
 bool checkStringPallindrome(string inputString) {
-	//if (inputString.length() % 2 == 1) {
-		for (int i = 0; i < inputString.length()/2; i++) {
-			if (inputString.at[i] != inputString.at[inputString.length() - 1 - i]) {
-				return false;
-			}
+	for (int i = 0; i < (inputString.length() / 2); i++) {
+		if (inputString.at(i) != inputString.at(inputString.length() - 1 - i)) {
+			return false;
 		}
-	//}
-	//else {
-
-	//}
-		return true;
+	}
+	return true;
 }
-int main(){
+int main() {
 
-	string input = "";
-	std::getline(std::cin, input);
-	std::cout<<checkStringPallindrome(input);
+	string input;
+	getline(cin, input);
+	if (checkStringPallindrome(input)) {
+		cout << "true" << endl;
+	}
+	else {
+		cout << "false" << endl;
+	}
 
 	system("pause");
 	return 1;
