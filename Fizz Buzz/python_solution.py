7defd4b5edc5ddaff223a29a6c48e9bc4505d032
nidhisebastian008 <nidcod@gmail.com>
2019-10-04 02:59:35 +0530

    add cycle sort

--- Fizz Buzz/python_solution.py
+++
@@ -1,9 +0,0 @@
-for x in range(1, 100):
-    if (x % 15 == 0):
-        print "fizzbuzz"
-    elif x % 3 == 0:
-        print "fizz"
-    elif x % 5 == 0:
-        print "buzz"
-    else:
-        print x
