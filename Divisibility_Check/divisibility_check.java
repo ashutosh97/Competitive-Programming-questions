4c47e121ea55fba6f879addfa0c56c780bbb7a84
jyotishman1207 <56396615+jyotishman1207@users.noreply.github.com>
2019-10-17 19:39:19 +0530

    Create Divisibility_Check.java
    New Code

---
+++ Divisibility_Check/divisibility_check.java
@@ -0,0 +1,17 @@
+class divi
+{
+    public static main(int n{})
+    {
+        int l=n.length;int c=0;
+        for(int i=0;i<l;i++)
+        {
+            for(int j=0;j<l;j++)
+            {
+                if(n(j)%n(i)==0)
+                c++;
+            }
+            if(c==l-1)
+            System.out.println("The buoy number is:"+n(i));
+        }
+    }
+}
