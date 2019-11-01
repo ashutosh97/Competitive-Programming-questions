e2a4ab941522476f02ecf0be38a43826f1186340
Tavien Floyd <tavienta@yahoo.com>
2019-10-31 22:37:25 -0400

    Compound interest C++

--- Leap Year/ruby_solution.rb
+++
@@ -1,8 +0,0 @@
-def leap(year)
-  puts year %400 == 0 || year %4 == 0 && !(year %100 == 0)
-end
-
-leap(1996) #expect true
-leap(2000) #expect true
-leap(2015) #expect false
-leap(1800) #expect false
--- ToyObsession/problem.c
+++
@@ -1,7 +0,0 @@
-We are to input a string of characters(say, s1= XxxyXxxXXxY) and a character(say, X) that belongs to that string
-( The string represents the collection of toys and the character represents the most beautiful toy)
- we are to input a number that denotes the no. of beautiful toys needed ( smaller than equal to the maximum beautiful toys, say 2 )
-	now we are to print the smallet length (as in no. of characters) of the substring
-eg here 2 X containing substrigs are... XxxyX  XxxX XX 
-       					 5	4    2
-					 max         min therefor 2 printed. \(>o<)/
